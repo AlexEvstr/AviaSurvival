0133c4e9d36d3b2005f0e66ff17575446f30b352
Alexander Evstratov <alexdrugev@me.com>
2024-04-03 16:21:14 +0300

    Add joystick, player movement with rotation towards direction.

---
+++ Assets/_Scripts/PlaneController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    private float _speed = 5.0f;
+    private float _rotationSpeed = 5;
+    public FixedJoystick fixJS;
+    public Rigidbody2D rb;
+
+    public void FixedUpdate()
+    {
+        Vector2 direction = Vector2.up * fixJS.Vertical + Vector2.right * fixJS.Horizontal;
+
+        float inputMagnitude = Mathf.Clamp01(direction.magnitude);
+        direction.Normalize();
+
+        transform.Translate(direction * _speed * inputMagnitude * Time.deltaTime, Space.World);
+
+        if (direction != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed );
+        }
+    }
+}
